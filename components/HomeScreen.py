d616cd5ca96e3cd2b14f54996ff3966311deeb57
= <=>
2022-10-20 07:54:34 +0800

    Added a json loader

--- components/HomeScreen.py
+++ components/HomeScreen.py
@@ -1,24 +1,38 @@
 from kivy.clock import Clock
 from kivy.network.urlrequest import UrlRequest
 from kivymd.uix.screen import MDScreen
-import certifi
+import certifi, json
+
+
 from api import API_KEY
 
 class HomeScreen(MDScreen):
     def __init__(self, **kw):
         super().__init__(**kw)
         Clock.schedule_once(self.set_toolbar_font)
-        self.get_news()
+        # Clock.schedule_once(self.get_news)
+        
         
     def set_toolbar_font(self, *args):
         self.ids.toolbar.ids.label_title.color = "#000000"
         self.ids.toolbar.ids.label_title.font_style = "Body1"
         self.ids.toolbar.ids.label_title.font_name = "Poppins"
     
-    def get_news(self):
+    def get_json_news(self):
+    	with open("results.json", "r", encoding='utf-8') as file:
+    		data = json.load(file)
+    	an_article = data['articles'][0]
+    
+    def get_news(self, *args):
         url = f"https://newsapi.org/v2/everything?q=bitcoin&apiKey={API_KEY}"
         headers = {'User-Agent': 'My Agent'}
         self.request = UrlRequest(url, self.res, ca_file=certifi.where(), verify=True, req_headers=headers)
+    
+    # Save to avoid calling api multiple times
+    def save_json_test(self):
+    	with open("results.json", "w",encoding='utf-8') as file:
+    		json.dump(self.request.result, file, ensure_ascii=False, indent=4)
 
     def res(self,*args):
         print("Result: after success", self.request.result)
+        self.save_json_test()
