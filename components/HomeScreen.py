12e40e7be97d36abf5bdc96a0fe3d5eef89445b3
Rui Yan <angruiyan@gmail.com>
2022-10-19 23:59:53 +0800

    Fixed urlrequest

--- components/HomeScreen.py
+++ components/HomeScreen.py
@@ -1,28 +1,23 @@
 from kivy.clock import Clock
 from kivy.network.urlrequest import UrlRequest
-
 from kivymd.uix.screen import MDScreen
+
+from api import API_KEY
 
 class HomeScreen(MDScreen):
     def __init__(self, **kw):
         super().__init__(**kw)
         Clock.schedule_once(self.set_toolbar_font)
-        Clock.schedule_once(self.get_news)
+        url = f"https://newsapi.org/v2/everything?q=bitcoin&apiKey={API_KEY}"
+        print(url)
+        self.request = UrlRequest(url, self.res)
+        print(self.request)
+        print("Result: before success", self.request.result,"\n")
         
     def set_toolbar_font(self, *args):
         self.ids.toolbar.ids.label_title.color = "#000000"
         self.ids.toolbar.ids.label_title.font_style = "Body1"
         self.ids.toolbar.ids.label_title.font_name = "Poppins"
-    
-    def on_start(self, **kwargs):
-    	print("hi")
-    
-    def get_news(self, *args):   
-        url = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=7c97ff4614c24b11a0f272c78e511a36"
-        print(url)
-        self.req = UrlRequest(url, self.res)
-        
-    def res(self, *args):
-        print("yes")
-        print(self.req.result)
-        self.ids.label.text = self.req.result
+
+    def res(self,*args):
+        print("Result: after success", self.request.result)
