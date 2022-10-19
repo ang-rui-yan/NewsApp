441305a80eff01f152c8404a056787ba11e0dc14
= <=>
2022-10-20 07:29:27 +0800

    Fixed UrlRequest. Added buildozer and requirements

--- components/HomeScreen.py
+++ components/HomeScreen.py
@@ -8,16 +8,17 @@ class HomeScreen(MDScreen):
     def __init__(self, **kw):
         super().__init__(**kw)
         Clock.schedule_once(self.set_toolbar_font)
-        url = f"https://newsapi.org/v2/everything?q=bitcoin&apiKey={API_KEY}"
-        print(url)
-        self.request = UrlRequest(url, self.res, ca_file=certifi.where())
-        print(self.request)
-        print("Result: before success", self.request.result,"\n")
+        self.get_news()
         
     def set_toolbar_font(self, *args):
         self.ids.toolbar.ids.label_title.color = "#000000"
         self.ids.toolbar.ids.label_title.font_style = "Body1"
         self.ids.toolbar.ids.label_title.font_name = "Poppins"
+    
+    def get_news(self):
+        url = f"https://newsapi.org/v2/everything?q=bitcoin&apiKey={API_KEY}"
+        headers = {'User-Agent': 'My Agent'}
+        self.request = UrlRequest(url, self.res, ca_file=certifi.where(), verify=True, req_headers=headers)
 
     def res(self,*args):
         print("Result: after success", self.request.result)
