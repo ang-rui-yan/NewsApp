82659475678c409939d9686365415f9ab252cb14
Rui Yan <angruiyan@gmail.com>
2022-10-20 10:47:15 +0800

    Added RecycleView. Testing feel on phone

--- components/HomeScreen.py
+++ components/HomeScreen.py
@@ -3,13 +3,14 @@ from kivy.network.urlrequest import UrlRequest
 from kivymd.uix.screen import MDScreen
 import certifi, json
 
+from components.ArticleRV import ArticleRV
 
 from api import API_KEY
 
 class HomeScreen(MDScreen):
     def __init__(self, **kw):
         super().__init__(**kw)
-        Clock.schedule_once(self.set_toolbar_font)
+        #Clock.schedule_once(self.set_toolbar_font)
         # Clock.schedule_once(self.get_news)
         
         
@@ -19,9 +20,9 @@ class HomeScreen(MDScreen):
         self.ids.toolbar.ids.label_title.font_name = "Poppins"
     
     def get_json_news(self):
-    	with open("results.json", "r", encoding='utf-8') as file:
-    		data = json.load(file)
-    	an_article = data['articles'][0]
+        with open("results.json", "r", encoding='utf-8') as file:
+            data = json.load(file)
+        an_article = data['articles'][0]
     
     def get_news(self, *args):
         url = f"https://newsapi.org/v2/everything?q=bitcoin&apiKey={API_KEY}"
@@ -31,7 +32,7 @@ class HomeScreen(MDScreen):
     # Save to avoid calling api multiple times
     def save_json_test(self):
     	with open("results.json", "w",encoding='utf-8') as file:
-    		json.dump(self.request.result, file, ensure_ascii=False, indent=4)
+            json.dump(self.request.result, file, ensure_ascii=False, indent=4)
 
     def res(self,*args):
         print("Result: after success", self.request.result)
